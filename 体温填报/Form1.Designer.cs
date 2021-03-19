bc3774833329654fc983662e1bb4ca8ed3cd0009
hanmoliuxuan <hanmoliuxuan@163.com>
2021-03-19 22:34:30 +0800

    体温1.0

---
+++ 体温填报/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace 体温填报
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label0 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.idnumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.temp = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label0
+            // 
+            this.label0.AutoSize = true;
+            this.label0.Location = new System.Drawing.Point(10, 531);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(41, 12);
+            this.label0.TabIndex = 0;
+            this.label0.Text = "身份证";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 526);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "提交";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // idnumber
+            // 
+            this.idnumber.Location = new System.Drawing.Point(57, 528);
+            this.idnumber.Name = "idnumber";
+            this.idnumber.Size = new System.Drawing.Size(147, 21);
+            this.idnumber.TabIndex = 2;
+            this.idnumber.Tag = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 531);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(245, 528);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 21);
+            this.name.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(435, 531);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "体温";
+            // 
+            // temp
+            // 
+            this.temp.Location = new System.Drawing.Point(470, 528);
+            this.temp.Name = "temp";
+            this.temp.Size = new System.Drawing.Size(55, 21);
+            this.temp.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(676, 487);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(351, 528);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "打开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 573);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.temp);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.idnumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label0);
+            this.Name = "Form1";
+            this.Text = "无锡体温填报1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox idnumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox temp;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 体温填报/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+namespace 体温填报
+{
+    public partial class Form1 : Form
+    {
+        string path = Application.StartupPath + @"\1.txt";
+        bool isopen = false;
+        public Form1()
+        {
+            InitializeComponent();
+            FileStream fs = File.Open(path, FileMode.OpenOrCreate);
+            fs.Close();
+            LoadInfo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isopen)
+            {
+                HtmlElement input_text = null;
+                HtmlElementCollection input_list = webBrowser1.Document.GetElementsByTagName("input");
+                input_text = input_list[1];
+                input_text.SetAttribute("value", temp.Text);
+
+                HtmlElementCollection button_list = webBrowser1.Document.GetElementsByTagName("button");
+                HtmlElement btn_sub = button_list[0];
+                btn_sub.InvokeMember("click");
+
+                SaveInfo();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://yiqing.zyyj.com.cn/h5_body_tw?user_type=2&id_card=" + idnumber.Text + "&user_name=" + name.Text);
+            isopen = true;
+        }
+
+        private void SaveInfo()
+        {
+            StreamWriter sw = new StreamWriter(path);
+
+            sw.WriteLine(idnumber.Text);
+            sw.WriteLine(name.Text);
+            sw.WriteLine(temp.Text);
+            sw.Close();
+        }
+        private void LoadInfo()
+        {
+            StreamReader sw = new StreamReader(path);
+            idnumber.Text = sw.ReadLine();
+            name.Text = sw.ReadLine();
+            temp.Text = sw.ReadLine();
+            sw.Close();
+        }
+    }
+}
